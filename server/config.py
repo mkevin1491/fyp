240540c843c23e9728b97de2e009a771b8fa75cf
MohammedAlshami <alshamicourses@gmail.com>
2024-07-01 01:20:59 +0800

    a

--- server/config.py
+++ server/config.py
@@ -5,6 +5,6 @@ load_dotenv()  # Load environment variables from .env file
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:83be03823fb8a7eea27a81dd0f1fc3cf91b2a2a9bfe89cda@64.227.116.49:3306/fyp'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:789a94d2bf04ae849e4d536a795563b3026543c9d2552782@159.89.31.77:3306/fyp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
